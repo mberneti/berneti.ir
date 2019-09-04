b93702ba3244ed850c506bb0bfc54ba3eea38a7a
Mohammadreza Berneti <mberneti@live.com>
2019-09-05 01:12:21 +0430

    init

---
+++ src/components/Footer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { rhythm } from '../utils/typography';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer
+        style={{
+          marginTop: rhythm(2.5),
+          paddingTop: rhythm(1),
+        }}
+      >
+        <div style={{ float: 'right' }}>
+          <a href="/rss.xml" target="_blank" rel="noopener noreferrer">
+            rss
+          </a>
+        </div>
+        <a
+          href="https://mobile.twitter.com/mberneti"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          twitter
+        </a>{' '}
+        &bull;{' '}
+        <a
+          href="https://github.com/mberneti"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>{' '}
+        &bull;{' '}
+        <a
+          href="https://stackoverflow.com/users/3464834/mohammadreza-berneti"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          stack overflow
+        </a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/pages/404.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <Layout location={this.props.location}>
+        <main>
+          <h1>Not Found</h1>
+          <p>I haven’t written this post yet. Will you help me write it?</p>
+        </main>
+      </Layout>
+    );
+  }
+}
+
+export default NotFoundPage;
